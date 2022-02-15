c512bc9fe9d898f5f9588135cd9d16a7600713ec
DanBaDo <byte4life@pm.me>
2022-02-15 13:24:10 +0100

    Adds exercise 1

---
+++ src/exercise1/script.js
@@ -0,0 +1,13 @@
+function app () {
+    const app = document.querySelector("app");
+    const solution = document.createElement("p");
+    const numbers = [22,1,4,3,5,7,8,9,12,11];
+    let result = [];
+
+    for (item of numbers) {
+        // Put your code here
+    }
+
+    solution.innerText = result;
+    app.appendChild(solution);
+}
