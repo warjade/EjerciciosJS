3d6d6225f161064836052a0047bd19102a8a7e54
DanBaDo <byte4life@pm.me>
2022-02-18 07:48:52 +0100

    Removes forgoted function

--- src/exercise1/index.js
+++ src/exercise1/index.js
@@ -1,15 +1,13 @@
-function app () {
-    const numbers = [22,1,4,3,5,7,8,9,12,11];
-    const odd = [];
-    const even = []
+const numbers = [22,1,4,3,5,7,8,9,12,11];
+const odd = [];
+const even = []
 
-    for (let item of numbers) {
-        console.log("item:", item);
+for (let item of numbers) {
+    console.log("item:", item);
 
-        // Put your code here
+    // Put your code here
 
-    }
-
-    console.log("odd:", odd);
-    console.log("even:", even);
 }
+
+console.log("odd:", odd);
+console.log("even:", even);
