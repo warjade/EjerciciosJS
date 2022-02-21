db959f77f46b935d0f3b7080b5162a21a5ac1541
DanBaDo <byte4life@pm.me>
2022-02-21 14:11:19 +0100

    Introduces if example

---
+++ src/for and if/exercise5/example.js
@@ -0,0 +1,8 @@
+const numbers = [10,11,12,13,14,15,16];
+const minimun = 13;
+
+for ( item of numbers ) {
+    if ( item >= minimun ) {
+        console.log(`${item} is greater or equal to ${minimun}`)
+    }
+}
