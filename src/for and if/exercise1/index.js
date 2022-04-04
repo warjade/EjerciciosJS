80b2a41ad381ef130f0448e313a0ba61c66606db
warjade <warjade@yahoo.es>
2022-04-04 13:16:05 +0200

    Ejercicio 1

--- src/for and if/exercise1/index.js
+++ src/for and if/exercise1/index.js
@@ -1,17 +1,17 @@
-const numbers = [22,1,4,3,5,7,8,9,12,11];
-const odd = []; //impar
-const even = []; //par
+const numbers = [22, 1, 4, 3, 5, 7, 8, 9, 12, 11];
+const odd = [];
+const even = []
 
 for (let item of numbers) {
     console.log("item:", item);
-    if (item%2 === 0) {
+
+    if (item % 2 === 0) {
         even.push(item);
     } else {
         odd.push(item);
     }
 
-
 }
 
-console.log("Impares:", odd);
-console.log("Pares:", even);
+console.log("odd:", odd);
+console.log("even:", even);
